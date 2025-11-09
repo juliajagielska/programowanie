77b439c96fd7ba25cae51c34bfe6525d86de7b94
juliajagielska <julia240406@gmail.com>
2025-11-09 17:48:16 +0100

    Zadanie 1 - funkcja z listą imion

---
+++ zad_1.py
@@ -0,0 +1,8 @@
+# Funkcja, która otrzymuje listę imion i wypisuje każde z nich
+def wyswietl_imiona(lista_imion):
+    for imie in lista_imion:
+        print(imie)
+
+# Przykładowe wywołanie funkcji z listą 5 imion
+imiona = ["Anna", "Bartek", "Celina", "Daniel", "Ewa"]
+wyswietl_imiona(imiona)
