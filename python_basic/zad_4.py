321788a3f28cbbd80ae1ef35e1a4c47fd6f67109
juliajagielska <julia240406@gmail.com>
2025-11-23 20:10:07 +0100

    Zadanie 4 - suma dwóch pierwszych liczb

---
+++ python_basic/zad_4.py
@@ -0,0 +1,9 @@
+def sprawdz_sume(a: int, b: int, c: int) -> bool:
+    return a + b >= c
+
+wynik: bool = sprawdz_sume(3, 5, 7)
+
+if wynik:
+    print("Suma dwóch pierwszych liczb jest większa lub równa trzeciej")
+else:
+    print("Suma dwóch pierwszych liczb jest mniejsza od trzeciej")
