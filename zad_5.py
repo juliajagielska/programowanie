460b5eabdb4a825ff75125412987f4810d4fe621
Julia Jagielska <julia240406@gmail.com>
2025-12-15 10:42:12 +0100

    Remove Python basic tasks from python_api

--- zad_5.py
+++
@@ -1,9 +0,0 @@
-# Funkcja, która otrzymuje listę 10 liczb i wyświetla co drugi element
-
-def co_drugi_element(lista_liczb):
-    for i in range(0, len(lista_liczb), 2):
-        print(lista_liczb[i])
-
-# Przykładowe wywołanie funkcji
-liczby = list(range(1, 11))
-co_drugi_element(liczby)
